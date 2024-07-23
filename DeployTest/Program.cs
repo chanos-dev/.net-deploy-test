2927f4d72fb5daeb32502a69b3bc2da3ff078fe6
chanos-dev <chanos.dev@gmail.com>
2024-07-23 23:07:31 +0900

    test proj

---
+++ DeployTest/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.WebHost.UseUrls("http://0.0.0.0:3000");
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
+
+app.Run();
